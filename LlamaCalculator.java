15405d15d01d8284828bfa642b75c4f0dafd3a30
Marcos-Gil <m.gil@live.ca>
2018-10-30 00:05:38 -0400

    Variable added for proximity instead of hardcoded value

--- LlamaCalculator.java
+++ LlamaCalculator.java
@@ -12,6 +12,7 @@ public class LlamaCalculator {
     Random rand2 = new Random();
     int numberOfSimulations = 100000;
     int countCloseEncounter = 0;
+    int maxProximity = 11;
     
     for (int x = 0; x < numberOfSimulations; x++){
       
@@ -22,21 +23,21 @@ public class LlamaCalculator {
       int x3 = rand2.nextInt(1001) + 1;
       int y3 = rand2.nextInt(1001) + 1;
       
-      if ((Math.abs(x1 - x2) < 11) && (Math.abs(y1 - y2) < 11)){
+      if ((Math.abs(x1 - x2) < maxProximity) && (Math.abs(y1 - y2) < maxProximity)){
       
         System.out.println("They spawned together in game #"+ x +"! Llama 1: (" + x1 + "," + y1 +"), Llama 2: (" + x2 + "," + y2 +")");
         
         countCloseEncounter++;
       }
       
-       if ((Math.abs(x1 - x3) < 11) && (Math.abs(y1 - y3) < 11)){
+       if ((Math.abs(x1 - x3) < maxProximity) && (Math.abs(y1 - y3) < maxProximity)){
       
         System.out.println("They spawned together in game #"+ x +"! Llama 1: (" + x1 + "," + y1 +"), Llama 3: (" + x3 + "," + y3 +")");
         
         countCloseEncounter++;
       }
        
-       if ((Math.abs(x2 - x3) < 11) && (Math.abs(y2 - y3) < 11)){
+       if ((Math.abs(x2 - x3) < maxProximity) && (Math.abs(y2 - y3) < maxProximity)){
       
         System.out.println("They spawned together in game #"+ x +"! Llama 2: (" + x2 + "," + y2 +"), Llama 3: (" + x3 + "," + y3 +")");
         
