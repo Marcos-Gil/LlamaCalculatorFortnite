451e90a156b7ee16631e41a3dd4fde4d100cb0d3
Marcos-Gil <m.gil@live.ca>
2018-10-29 23:54:45 -0400

    Probability calculator

---
+++ LlamaCalculator.java
@@ -0,0 +1,49 @@
+/**
+ * By: Marcos Gil
+ */
+import java.util.*;
+import java.math.*;
+
+public class LlamaCalculator {
+  
+  public static void main(String[] args) { 
+    
+    Random rand = new Random();
+    Random rand2 = new Random();
+    int numberOfSimulations = 100000;
+    int countCloseEncounter = 0;
+    
+    for (int x = 0; x < numberOfSimulations; x++){
+      
+      int x1 = rand.nextInt(1001) + 1;
+      int y1 = rand.nextInt(1001) + 1;
+      int x2 = rand2.nextInt(1001) + 1;
+      int y2 = rand2.nextInt(1001) + 1;
+      int x3 = rand2.nextInt(1001) + 1;
+      int y3 = rand2.nextInt(1001) + 1;
+      
+      if ((Math.abs(x1 - x2) < 11) && (Math.abs(y1 - y2) < 11)){
+      
+        System.out.println("They spawned together in game #"+ x +"! Llama 1: (" + x1 + "," + y1 +"), Llama 2: (" + x2 + "," + y2 +")");
+        
+        countCloseEncounter++;
+      }
+      
+       if ((Math.abs(x1 - x3) < 11) && (Math.abs(y1 - y3) < 11)){
+      
+        System.out.println("They spawned together in game #"+ x +"! Llama 1: (" + x1 + "," + y1 +"), Llama 3: (" + x3 + "," + y3 +")");
+        
+        countCloseEncounter++;
+      }
+       
+       if ((Math.abs(x2 - x3) < 11) && (Math.abs(y2 - y3) < 11)){
+      
+        System.out.println("They spawned together in game #"+ x +"! Llama 2: (" + x2 + "," + y2 +"), Llama 3: (" + x3 + "," + y3 +")");
+        
+        countCloseEncounter++;
+      }
+    }
+    
+    System.out.println("They spawned together " + countCloseEncounter + " time(s) in " + numberOfSimulations + " matches.");
+  }
+}
